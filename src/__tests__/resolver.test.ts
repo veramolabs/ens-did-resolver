74318ee71ae995a2f4e5e1631171286a29329fcd
nickreynolds <nicholas.s.reynolds@gmail.com>
2022-11-02 19:24:12 -0400

    fix: remove deprecated testnets
    test: add test for mismatched network

--- src/__tests__/resolver.test.ts
+++ src/__tests__/resolver.test.ts
@@ -8,39 +8,34 @@ describe('ensResolver', () => {
   it('works with single, unnamed network', async () => {
     expect.assertions(1)
     let didResolver: Resolvable = new Resolver(
-      getResolver({ rpcUrl: 'https://ropsten.infura.io/v3/e471b8639c314004ae67ec0078f70102' })
+      getResolver({ rpcUrl: 'https://mainnet.infura.io/v3/e471b8639c314004ae67ec0078f70102' })
     )
-    const did = 'did:ens:oliver-rop.eth'
+    const did = 'did:ens:vitalik.eth'
+    const ethrAddr = '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045'
     const resolutionResult = await didResolver.resolve(did)
     expect(resolutionResult).toEqual<DIDResolutionResult>({
       didDocument: {
         id: did,
         service: [
           {
-            id: `${did}#Web3PublicProfile-0x773230C8719783C18147F18BbB8340a347196eE5`,
+            id: `${did}#Web3PublicProfile-${ethrAddr}`,
             type: 'Web3PublicProfile',
-            serviceEndpoint: 'oliver-rop.eth',
+            serviceEndpoint: 'vitalik.eth',
           },
         ],
         verificationMethod: [
           {
-            id: `${did}#0x773230C8719783C18147F18BbB8340a347196eE5`,
+            id: `${did}#${ethrAddr}`,
             type: 'EcdsaSecp256k1RecoveryMethod2020',
             controller: did,
-            blockchainAccountId: '0x773230C8719783C18147F18BbB8340a347196eE5@eip155:3',
-          },
-          {
-            id: `${did}#my-id`,
-            publicKeyMultibase: 'z9hFgmPVfmBZwRvFEyniQDBkz9LmV7gDEqytWyGZLmDXE',
-            controller: did,
-            type: 'X25519KeyAgreementKey2019',
+            blockchainAccountId: `${ethrAddr}@eip155:1`,
           },
         ],
-        authentication: [`${did}#0x773230C8719783C18147F18BbB8340a347196eE5`],
-        capabilityDelegation: [`${did}#0x773230C8719783C18147F18BbB8340a347196eE5`],
-        capabilityInvocation: [`${did}#0x773230C8719783C18147F18BbB8340a347196eE5`],
-        assertionMethod: [`${did}#0x773230C8719783C18147F18BbB8340a347196eE5`],
-        keyAgreement: [`${did}#my-id`],
+        authentication: [`${did}#${ethrAddr}`],
+        capabilityDelegation: [`${did}#${ethrAddr}`],
+        capabilityInvocation: [`${did}#${ethrAddr}`],
+        assertionMethod: [`${did}#${ethrAddr}`],
+        keyAgreement: [],
       },
       didDocumentMetadata: {},
       didResolutionMetadata: { contentType: 'application/did+json' },
@@ -50,74 +45,106 @@ describe('ensResolver', () => {
   it('works with single, named network', async () => {
     expect.assertions(1)
     let didResolver: Resolvable = new Resolver(
-      getResolver({ name: 'ropsten', rpcUrl: 'https://ropsten.infura.io/v3/e471b8639c314004ae67ec0078f70102' })
+      getResolver({ name: 'goerli', rpcUrl: 'https://goerli.infura.io/v3/e471b8639c314004ae67ec0078f70102' })
     )
-    const did = 'did:ens:ropsten:oliver-rop.eth'
-    const result = await didResolver.resolve(did)
-    expect(result.didDocument).toEqual<DIDDocument>({
-      id: did,
-      service: [
-        {
-          id: `${did}#Web3PublicProfile-0x773230C8719783C18147F18BbB8340a347196eE5`,
-          type: 'Web3PublicProfile',
-          serviceEndpoint: 'oliver-rop.eth',
-        },
-      ],
-      verificationMethod: [
-        {
-          id: `${did}#0x773230C8719783C18147F18BbB8340a347196eE5`,
-          type: 'EcdsaSecp256k1RecoveryMethod2020',
-          controller: did,
-          blockchainAccountId: '0x773230C8719783C18147F18BbB8340a347196eE5@eip155:3',
-        },
-        {
-          id: `${did}#my-id`,
-          publicKeyMultibase: 'z9hFgmPVfmBZwRvFEyniQDBkz9LmV7gDEqytWyGZLmDXE',
-          controller: did,
-          type: 'X25519KeyAgreementKey2019',
-        },
-      ],
-      authentication: [`${did}#0x773230C8719783C18147F18BbB8340a347196eE5`],
-      capabilityDelegation: [`${did}#0x773230C8719783C18147F18BbB8340a347196eE5`],
-      capabilityInvocation: [`${did}#0x773230C8719783C18147F18BbB8340a347196eE5`],
-      assertionMethod: [`${did}#0x773230C8719783C18147F18BbB8340a347196eE5`],
-      keyAgreement: [`${did}#my-id`],
+    const did = 'did:ens:goerli:whatever.eth'
+    const ethrAddr = '0x4af859d61d07A8c515FE0E3Cc1Ea5e49A260bBa3'
+    const resolutionResult = await didResolver.resolve(did)
+    expect(resolutionResult).toEqual<DIDResolutionResult>({
+      didDocument: {
+        id: did,
+        service: [
+          {
+            id: `${did}#Web3PublicProfile-${ethrAddr}`,
+            type: 'Web3PublicProfile',
+            serviceEndpoint: 'whatever.eth',
+          },
+        ],
+        verificationMethod: [
+          {
+            id: `${did}#${ethrAddr}`,
+            type: 'EcdsaSecp256k1RecoveryMethod2020',
+            controller: did,
+            blockchainAccountId: `${ethrAddr}@eip155:5`,
+          },
+          {
+            id: `${did}#my-key`,
+            type: 'X25519KeyAgreementKey2019',
+            controller: did,
+            publicKeyMultibase: 'z9hFgmPVfmBZwRvFEyniQDBkz9LmV7gDEqytWyGZLmDXE'
+          }
+        ],
+        authentication: [`${did}#${ethrAddr}`],
+        capabilityDelegation: [`${did}#${ethrAddr}`],
+        capabilityInvocation: [`${did}#${ethrAddr}`],
+        assertionMethod: [`${did}#${ethrAddr}`],
+        keyAgreement: [`${did}#my-key`],
+      },
+      didDocumentMetadata: {},
+      didResolutionMetadata: { contentType: 'application/did+json' },
     })
   })
 
   it('works with infura', async () => {
     expect.assertions(1)
     let didResolver: Resolvable = new Resolver(getResolver({ infuraProjectId: 'e471b8639c314004ae67ec0078f70102' }))
-    const did = 'did:ens:ropsten:oliver-rop.eth'
-    const result = await didResolver.resolve(did)
-    expect(result.didDocument).toEqual<DIDDocument>({
-      id: did,
-      service: [
-        {
-          id: `${did}#Web3PublicProfile-0x773230C8719783C18147F18BbB8340a347196eE5`,
-          type: 'Web3PublicProfile',
-          serviceEndpoint: 'oliver-rop.eth',
-        },
-      ],
-      verificationMethod: [
-        {
-          id: `${did}#0x773230C8719783C18147F18BbB8340a347196eE5`,
-          type: 'EcdsaSecp256k1RecoveryMethod2020',
-          controller: did,
-          blockchainAccountId: '0x773230C8719783C18147F18BbB8340a347196eE5@eip155:3',
-        },
-        {
-          id: `${did}#my-id`,
-          publicKeyMultibase: 'z9hFgmPVfmBZwRvFEyniQDBkz9LmV7gDEqytWyGZLmDXE',
-          controller: did,
-          type: 'X25519KeyAgreementKey2019',
-        },
-      ],
-      authentication: [`${did}#0x773230C8719783C18147F18BbB8340a347196eE5`],
-      capabilityDelegation: [`${did}#0x773230C8719783C18147F18BbB8340a347196eE5`],
-      capabilityInvocation: [`${did}#0x773230C8719783C18147F18BbB8340a347196eE5`],
-      assertionMethod: [`${did}#0x773230C8719783C18147F18BbB8340a347196eE5`],
-      keyAgreement: [`${did}#my-id`],
+    const did = 'did:ens:goerli:whatever.eth'
+    const ethrAddr = '0x4af859d61d07A8c515FE0E3Cc1Ea5e49A260bBa3'
+    const resolutionResult = await didResolver.resolve(did)
+    expect(resolutionResult).toEqual<DIDResolutionResult>({
+      didDocument: {
+        id: did,
+        service: [
+          {
+            id: `${did}#Web3PublicProfile-${ethrAddr}`,
+            type: 'Web3PublicProfile',
+            serviceEndpoint: 'whatever.eth',
+          },
+        ],
+        verificationMethod: [
+          {
+            id: `${did}#${ethrAddr}`,
+            type: 'EcdsaSecp256k1RecoveryMethod2020',
+            controller: did,
+            blockchainAccountId: `${ethrAddr}@eip155:5`,
+          },
+          {
+            id: `${did}#my-key`,
+            type: 'X25519KeyAgreementKey2019',
+            controller: did,
+            publicKeyMultibase: 'z9hFgmPVfmBZwRvFEyniQDBkz9LmV7gDEqytWyGZLmDXE'
+          }
+        ],
+        authentication: [`${did}#${ethrAddr}`],
+        capabilityDelegation: [`${did}#${ethrAddr}`],
+        capabilityInvocation: [`${did}#${ethrAddr}`],
+        assertionMethod: [`${did}#${ethrAddr}`],
+        keyAgreement: [`${did}#my-key`],
+      },
+      didDocumentMetadata: {},
+      didResolutionMetadata: { contentType: 'application/did+json' },
     })
+  })
+
+  it('works fails trying to goerli name on mainnet', async () => {
+    expect.assertions(1)
+    let didResolver: Resolvable = new Resolver(getResolver({ rpcUrl: 'https://mainnet.infura.io/v3/e471b8639c314004ae67ec0078f70102' }))
+    const did = 'did:ens:goerli:whatever.eth'
+    const resolutionResult = await didResolver.resolve(did)
+    expect(resolutionResult.didResolutionMetadata.error).toEqual("unknownNetwork")
+  })
+
+  it('multi provider config', async () => {
+    expect.assertions(2)
+    let didResolver: Resolvable = new Resolver(getResolver({ networks: [
+      { name: 'goerli', rpcUrl: 'https://goerli.infura.io/v3/e471b8639c314004ae67ec0078f70102' },
+      { rpcUrl: 'https://mainnet.infura.io/v3/e471b8639c314004ae67ec0078f70102' }
+    ]}))
+    const did = 'did:ens:goerli:whatever.eth'
+    const resolutionResult = await didResolver.resolve(did)
+    expect(resolutionResult.didDocument?.id).toEqual("did:ens:goerli:whatever.eth")
+
+    const resolutionResult2 = await didResolver.resolve('did:ens:vitalik.eth')
+    expect(resolutionResult2.didDocument?.id).toEqual('did:ens:vitalik.eth')
   })
 })
